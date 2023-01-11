06965b8f560db259a4727be4f22287c5d8cf1536
Eduardo Ruiz <eduardoruiz19@hotmail.com>
2023-01-11 06:17:31 -0500

    primer commit

---
+++ src/main/java/com/bytecode/core/ByteCode2Application.java
@@ -0,0 +1,13 @@
+package com.bytecode.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByteCode2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ByteCode2Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/bytecode/core/controllers/ControllerBasic.java
@@ -0,0 +1,42 @@
+package com.bytecode.core.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RequestMapping("/hola")
+public class ControllerBasic {
+	
+	@GetMapping(path = { "/", "/ndo"} )
+	public String saludar() {
+		return "index";
+	}
+
+	@GetMapping("/greetings")
+	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+		model.addAttribute("name", name);
+		return "greeting";
+	}
+	
+}
+
+/*
+ * 
+		<dependency>
+			<groupId>org.springframework.boot</groupId>
+			<artifactId>spring-boot-starter-data-jpa</artifactId>
+		</dependency>
+
+		<dependency>
+			<groupId>com.mysql</groupId>
+			<artifactId>mysql-connector-j</artifactId>
+			<scope>runtime</scope>
+		</dependency>
+
+ * 
+ * 
+ */
